967bbc84e73ad229a8951a4188857fe43d350721
onurdilsiz <onurdilsiz0@gmail.com>
2024-04-28 21:21:34 +0300

    SvgIcon component added

--- bibliosearch_web/src/pages/LoginPage.tsx
+++ bibliosearch_web/src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import SvgIcon from "../components/SvgIcon";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -25,14 +26,8 @@ const LoginPage = () => {
   return (
     <form className="flex flex-col gap-4 p-4" onSubmit={handleLogin}>
       <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-        </svg>
+       
+       <SvgIcon icon="user"></SvgIcon>
         <input
           type="text"
           className="grow"
@@ -42,18 +37,7 @@ const LoginPage = () => {
         />
       </label>
       <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-            clipRule="evenodd"
-          />
-        </svg>
+      <SvgIcon icon="password"></SvgIcon>
         <input
           type="password"
           className="grow"

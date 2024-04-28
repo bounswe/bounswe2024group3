4bee09e25619f051e84d7fd04d723d710d06e9b6
Ekrem BAL <mail.ekrembal@gmail.com>
2024-04-28 19:19:51 +0300

    Improve folder structure
    - Move App.tsx to pages/Home.tsx
    - Remove App.css, logo.svg, react-app-env.d.ts
    
    Co-authored-by: Onur Dilsiz <onurdilsiz0@gmail.com>

--- bibliosearch_web/src/index.tsx
+++ bibliosearch_web/src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );

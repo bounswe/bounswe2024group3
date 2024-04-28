d6c1d6bdd9f843d367608ee2f7fc53f937f0f93d
Onur Dilsiz <88006104+onurdilsiz@users.noreply.github.com>
2024-04-28 21:30:38 +0300

    Adds SvgIcon component (#113)
    * SvgIcon component added
    
    * removes unnecessary closing tabs

--- bibliosearch_web/src/pages/RegisterPage.tsx
+++ bibliosearch_web/src/pages/RegisterPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import SvgIcon from "../components/SvgIcon";
 
 const RegisterPage = () => {
   const [name, setName] = useState("");
@@ -28,14 +29,7 @@ const RegisterPage = () => {
   return (
     <form className="flex flex-col gap-4 p-4" onSubmit={handleLogin}>
       <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-        </svg>
+        <SvgIcon icon = "user"/>
         <input
           type="text"
           className="grow"
@@ -45,14 +39,8 @@ const RegisterPage = () => {
         />
       </label>
       <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-        </svg>
+      <SvgIcon icon = "user"/>
+
         <input
           type="text"
           className="grow"
@@ -62,15 +50,8 @@ const RegisterPage = () => {
         />
       </label>
       <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-          <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-        </svg>
+      <SvgIcon icon = "email"/>
+
         <input
           type="text"
           className="grow"
@@ -80,14 +61,8 @@ const RegisterPage = () => {
         />
       </label>
       <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-        </svg>
+      <SvgIcon icon = "user"/>
+
         <input
           type="text"
           className="grow"
@@ -97,18 +72,8 @@ const RegisterPage = () => {
         />
       </label>
       <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-            clipRule="evenodd"
-          />
-        </svg>
+      <SvgIcon icon = "password"/>
+
         <input
           type="password"
           className="grow"

8d8e2c16b7283fe8fb671f015d5e7dbdd2fc3784
Ahmet Burkay KINIK <aburkaykinik@gmail.com>
2024-05-15 21:06:21 +0300

    implemented the create_book function

--- bibliosearch_backend/api/views.py
+++ bibliosearch_backend/api/views.py
@@ -23,14 +23,74 @@ from django.contrib.auth.models import User
 from api.models import BiblioSearchUser
 
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 #import decorators
 from django.contrib.auth.decorators import login_required  
 
+from .models import Book, Author, Genre
+from datetime import datetime
 
 # Create your views here.
 
 from api.wikidata_client import search_book_by_keyword
+
+@csrf_exempt
+def create_post(request):
+    data = json.loads(request.body)
+    return create_book(data)
+
+
+def create_book(data):
+    try:
+
+        book_data = data.get('book')
+        title_data = data.get('title')
+        description_data = data.get('description')
+        authors_data = data.get('authors')
+        genres_data = data.get('genres')
+        isbn_data = data.get('ISBN13')
+        publication_year = data.get('publicationYear')
+
+        # Retrieve or create the author
+        author_name = authors_data['value']
+        first_name, last_name = author_name.split(maxsplit=1)  # Simplistic split; consider edge cases
+        author, created_author = Author.objects.get_or_create(name=first_name, surname=last_name)
+
+        # Retrieve or create the genre
+        genre_name = genres_data['value']
+        genre, created_genre = Genre.objects.get_or_create(name=genre_name)
+        
+        # Prepare book details
+        title = title_data['value']
+        description = description_data['value']
+        cover_url = book_data['value']
+        isbn = isbn_data['value']
+        # remove hyphens from ISBN
+        isbn = isbn.replace('-', '')
+
+        publication_date = datetime.strptime(publication_year['value'], "%Y").date()
+
+        # Create the book object
+        book, created_book = Book.objects.get_or_create(
+            title=title,
+            description=description,
+            cover_url=cover_url,
+            isbn=isbn,
+            publication_date=publication_date
+        )
+        book.authors.add(author)
+        book.genres.add(genre)
+        book.save()
+
+        if created_book:
+            return JsonResponse({'message': 'Book created successfully!'}, status=201)
+        else:
+            return JsonResponse({'message': 'Book already exists!'}, status=200)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
 
 @require_http_methods(["GET"])
 def search_book(request):

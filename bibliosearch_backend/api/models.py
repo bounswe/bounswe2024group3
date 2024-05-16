a304b1f94cb348a3e89cf21833aaf23be8f30e6b
batuhansolmaz <batuso123@gmail.com>
2024-05-16 12:24:08 +0300

    Added post model

--- bibliosearch_backend/api/models.py
+++ bibliosearch_backend/api/models.py
@@ -66,3 +66,17 @@ class BookList(models.Model):
         for book in books:
             self.books.add(book)
         self.save()
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f"Post by {self.user.username} at {self.created_at}"
+
+    @property
+    def total_likes(self):
+        return self.likes.count()
+

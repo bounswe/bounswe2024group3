3a520c495b423aa1c2b7289b1670c817eb6be29f
Ekrem BAL <mail.ekrembal@gmail.com>
2024-10-16 00:09:16 +0300

    Add tailwind, daisy ui, typescript, react app

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default App;

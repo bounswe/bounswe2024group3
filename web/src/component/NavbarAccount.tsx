e3e46d792f62baebe5b4950e6561c03951e77b07
Onur Dilsiz <onurdilsiz0@gmail.com>
2024-10-20 00:49:52 +0300

    add Header and navbar with login

---
+++ web/src/component/NavbarAccount.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { req } from "../utils/client";
+// import { useUser } from "../providers/UserContest";
+
+type NavbarAccountProps = {
+  username: string;
+};
+
+const NavbarAccount = ({ username }: NavbarAccountProps) => {
+  const navigate = useNavigate();
+//   const { setUsername, setError } = useUser();
+
+
+//   const logout = async () => {
+//     try {
+//       await req("logout", "post", {});
+//     } catch (error: any) {
+//       console.error("Logout failed:", error);
+//       setError(error.message);
+//     }
+//     setUsername("");
+//     navigate("/");
+//   };
+
+  if (username !== "") {
+    return (
+      <div className="dropdown dropdown-end">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-ghost btn-circle avatar"
+        >
+          <div className="w-10 rounded-full">
+            <img alt={username} src="https://i.pravatar.cc/300" />
+          </div>
+        </div>
+        <ul
+          tabIndex={0}
+          className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/settings">Settings</Link>
+          </li>
+          <li>
+            {/* <Link to="/" onClick={logout}>
+              Logout
+            </Link> */}
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <ul className="menu menu-horizontal px-1">
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    );
+  }
+};
+
+export default NavbarAccount;

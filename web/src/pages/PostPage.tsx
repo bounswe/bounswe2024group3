8b7d7db4bf2a46d6caf7563d719b54171a63362e
jorgeveji112 <163279678+jorgeveji112@users.noreply.github.com>
2024-10-20 16:46:37 +0300

    Creation of PostCard

--- web/src/pages/PostPage.tsx
+++ web/src/pages/PostPage.tsx
@@ -1,9 +1,24 @@
 import { useParams } from "react-router-dom";
 import SpotifyEmbed from "../components/SpotifyEmbed";
+import PostCard from "../components/PostCard";
+
 
 interface PostPageProps {
   type: string;
 }
+
+export type PostDetails = {
+  id: number;
+  imageUrl: string | null;
+  title: string;
+  content: string;
+  username: string;
+  likes: number;
+  dislikes: number;
+  created_at: Date,
+  type: string;
+  spotifyId: string;
+};
 
 const PostPage: React.FC<PostPageProps> = ({ type }) => {
   const { spotifyId } = useParams<{ spotifyId: string }>();
@@ -17,7 +32,19 @@ const PostPage: React.FC<PostPageProps> = ({ type }) => {
       <h1 className="text-3xl font-bold underline text-red-600">
         Post Page - {type.toUpperCase()}
       </h1>
-      <SpotifyEmbed spotifyId={`spotify:${type}:${spotifyId}?theme=0`} />
+      <PostCard
+        post={{
+          id: 1,
+          imageUrl: null,
+          title: "Post Title",
+          content: "This is the content of the post",
+          username: "Username",
+          likes: 10,
+          dislikes: 5,
+          created_at: new Date(),
+          type: "track",
+          spotifyId: spotifyId,
+        }} />
       <button className="btn btn-primary">Primary</button>
     </>
   );

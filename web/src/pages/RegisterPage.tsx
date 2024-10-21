5281c7026ce4a480ea2d99fb01572d46fe664f9c
Onur Dilsiz <88006104+onurdilsiz@users.noreply.github.com>
2024-10-21 05:27:54 +0300

    add axios request handler (#325)

--- web/src/pages/RegisterPage.tsx
+++ web/src/pages/RegisterPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import FormInput from "../components/FormInput";
 import { useNavigate } from "react-router-dom";
-// import { req } from "../utils/client";
+import { req } from "../utils/client";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
@@ -10,19 +10,30 @@ const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [labels, setLabels] = useState<string[]>([]);
+
   const [error, setError] = useState("");
+ 
+  const handleCheckboxChange = (value:any) => {
+    setLabels((prevLabels) =>
+      prevLabels.includes(value)
+        ? prevLabels.filter((label) => label !== value)
+        : [...prevLabels, value]
+    );
+  };
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setError("");
     try {
-    //   await req("register", "post", {
-    //     name: name,
-    //     surname: surname,
-    //     email: email,
-    //     username: username,
-    //     password: password,
-    //   });
+      await req('register' , 'post', {
+        name: name,
+        surname: surname,
+        email: email,
+        username: username,
+        password: password,
+        labels : labels
+      });
       navigate("/login");
     } catch (error: any) {
       setError(error.message);
@@ -36,6 +47,55 @@ const RegisterPage = () => {
       <FormInput icon= "email" type="email" placeholder ="Email" value ={email} onChange ={(e:any) => setEmail(e.target.value)} /> 
       <FormInput icon= "user" type="text" placeholder ="Username" value ={username} onChange ={(e:any) => setUsername(e.target.value)} /> 
       <FormInput icon= "password" type="password" placeholder ="Password" value ={password} onChange ={(e:any) => setPassword(e.target.value)} /> 
+      <p className="text-lg font-bold text-slate-950" >Select Roles </p>
+        <div >
+          <ul>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Artist"
+                  checked={labels.includes('Artist')}
+                  onChange={() => handleCheckboxChange('Artist')}
+                />
+                Artist
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Hobbyist"
+                  checked={labels.includes('Hobbyist')}
+                  onChange={() => handleCheckboxChange('Hobbyist')}
+                />
+                Hobbyist
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Listener"
+                  checked={labels.includes('Listener')}
+                  onChange={() => handleCheckboxChange('Listener')}
+                />
+                Listener
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Organizer"
+                  checked={labels.includes('Organizer')}
+                  onChange={() => handleCheckboxChange('Organizer')}
+                />
+                Organizer
+              </label>
+            </li>
+          </ul>
+        </div>
       <button type="submit" className="btn btn-primary mt-4">
         Register
       </button>

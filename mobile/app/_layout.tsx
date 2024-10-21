b19b4b5111126b04279eb6c1565e09cc2c20f924
ns <enes.besler@boun.edu.tr>
2024-10-22 02:05:35 +0300

    Rename project name on mobile-app.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ title: 'SpotOn' }} />
     </Stack>
   );
 }

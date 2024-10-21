bafa9dd7f35d5a2887c7f26e4a60a8dc4bb31e76
Ekrem BAL <mail.ekrembal@gmail.com>
2024-10-21 00:03:13 -0400

    Fix spotify embed bug with external library

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,6 @@
-import SpotifyEmbed from "./components/SpotifyEmbed";
-
 function App() {
   return (
     <>
-      
-      <SpotifyEmbed spotifyId="spotify:track:2LETLpcnlbL2d5IbnNYLf8?theme=0" />
     </>
   );
 }

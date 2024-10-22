7aeaf37310be54968c2c848730a4772705d1b83e
Abdusshh <abdullah.enes189@gmail.com>
2024-10-22 09:48:48 +0300

    Delete header and use expo-router

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,38 +1,42 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// app/_layout.tsx
+import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import MainScreen from './index';
-import ProfileScreen from './profile';
-import SearchScreen from './search';
-
-const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
   return (
-    <Tab.Navigator
-      initialRouteName="index"  // Set Main as the default page
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'index') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'profile') {
-            iconName = focused ? 'person' : 'person-outline';
-          } else if (route.name === 'search') {
-            iconName = focused ? 'search' : 'search-outline';
-          }
-
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
+    <Tabs
+      screenOptions={{
+        headerShown: false, // Hide the header if you don't want it
+        tabBarActiveTintColor: '#2f95dc',
       }}
     >
-      <Tab.Screen name="search" component={SearchScreen} options={{title: "Search"}}/>
-      <Tab.Screen name="index" component={MainScreen} options={{ title: 'Home' }} />
-      <Tab.Screen name="profile" component={ProfileScreen} options={{ title: 'Profile'}} />
-    </Tab.Navigator>
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" color={color} size={size} />
+          ),
+        }}
+      />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home-outline" color={color} size={size} />
+            ),
+          }}
+        />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }

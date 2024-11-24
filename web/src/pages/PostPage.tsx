527c0c3e5418916bd39b091e24d7cdfb7c97295d
Ekrem BAL <mail.ekrembal@gmail.com>
2024-11-24 23:29:43 +0300

    Add recommendations (#455)
    * Re-add recommendations
    
    * Fix recommendations

--- web/src/pages/PostPage.tsx
+++ web/src/pages/PostPage.tsx
@@ -6,7 +6,7 @@ import RecommendationItem from "../components/RecommendationItem";
 import { useUser } from "../providers/UserContext";
 import useAccessibility from "../components/Accessibility";
 import { PostDetails } from "./FeedPage";
-import { createSpotifyLink, req } from "../utils/client";
+import { createSpotifyLink, parseSpotifyLink, req } from "../utils/client";
 import CreatePostForm from "../components/CreatePostForm";
 
 interface PostPageProps {
@@ -16,6 +16,7 @@ interface PostPageProps {
 const PostPage: React.FC<PostPageProps> = ({ type }) => {
   const { spotifyId } = useParams<{ spotifyId: string }>();
   const [posts, setPosts] = useState<PostDetails[]>([]);
+  const [recommendations, setRecommendations] = useState<string[]>([]);
   const [newPostContent, setNewPostContent] = useState(""); // To track new post content
   const { username } = useUser();
 
@@ -39,7 +40,49 @@ const PostPage: React.FC<PostPageProps> = ({ type }) => {
         console.error("Failed to fetch posts:", error);
       }
     };
+
+    const addNowPlaying = async () => {
+      try {
+        if (!spotifyId) {
+          throw new Error("Invalid Spotify ID");
+        }
+        const storedLatitude = localStorage.getItem("latitude");
+        const storedLongitude = localStorage.getItem("longitude");
+        const response = await req("save-now-playing", "post", {
+          link: createSpotifyLink({ type, id: spotifyId }),
+          latitude: parseFloat(storedLatitude || "0"),
+          longitude: parseFloat(storedLongitude || "0"),
+        });
+        console.log("Added now playing:", response.data);
+      } catch (error: any) {
+        console.error("Failed to add now playing:", error);
+      }
+    };
+
+    const getRecommendations = async () => {
+      try {
+        const response = await req("get-posts/", "get", {});
+        console.log("Feed response:", response.data);
+        const posts: PostDetails[] = response.data.posts;
+
+        if (posts.length === 0) {
+          throw new Error("No posts found");
+        }
+
+        // Extract links, make them unique, and limit to 10
+        let links = posts.map((post) => post.content.link);
+        const uniqueLinks = Array.from(new Set(links)).slice(0, 10);
+
+        setRecommendations(uniqueLinks);
+        console.log("Recommendations response:", uniqueLinks);
+      } catch (error: any) {
+        console.error("Failed to fetch recommendations:", error);
+      }
+    };
+
     handleQuery();
+    addNowPlaying();
+    getRecommendations();
   }, [spotifyId]);
 
   if (!posts || !posts.length || !spotifyId) {
@@ -47,7 +90,7 @@ const PostPage: React.FC<PostPageProps> = ({ type }) => {
   }
 
   return (
-    <div className="flex  justify-center items-center">
+    <div className="flex justify-center">
       {/* Main Content Section */}
       <div className="flex-1  max-w-2xl w-full">
         <Spotify wide link={`https://open.spotify.com/${type}/${spotifyId}`} />
@@ -79,15 +122,21 @@ const PostPage: React.FC<PostPageProps> = ({ type }) => {
       </div>
 
       {/* Recommendations Bar */}
-      {/* <div className="w-64 bg-gray-100 p-4 ml-4">
+      <div className="w-64 bg-gray-100 p-4 ml-4">
         <h3 className="text-lg font-semibold mb-4">Recommended for You</h3>
-        {mockPosts
-          .filter((x) => x.spotifyId !== spotifyId && x.type === type)
+        {recommendations
+          .filter((x) => parseSpotifyLink(x).id !== spotifyId)
           .slice(0, 10)
           .map((rec) => (
-            <RecommendationItem key={rec.id} rec={rec} />
+            <RecommendationItem
+              key={parseSpotifyLink(rec).id}
+              rec={{
+                type: parseSpotifyLink(rec).type,
+                spotifyId: parseSpotifyLink(rec).id,
+              }}
+            />
           ))}
-      </div> */}
+      </div>
     </div>
   );
 };

e1220e772a8a4a47763cc49a85fc6a88fd2bdab4
ns <enes.besler@boun.edu.tr>
2024-11-25 16:36:16 +0300

    recommendations page added.

---
+++ mobile/app/(tabs)/recommendations.tsx
@@ -0,0 +1,53 @@
+// app/(tabs)/recommendations.tsx
+
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, FlatList, Text, TouchableOpacity, View } from 'react-native';
+import PostCard from '../../components/PostCard';
+import { mockPosts } from '../../pages/mockPosts';
+
+function Recommendations() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [randomPosts, setRandomPosts] = useState([]);
+
+  const toggleTheme = () => {
+    setIsDarkTheme((previousState) => !previousState);
+  };
+
+  useEffect(() => {
+    // Shuffle the posts and select the first three
+    const shuffledPosts = [...mockPosts].sort(() => 0.5 - Math.random());
+    setRandomPosts(shuffledPosts.slice(0, 3));
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: isDarkTheme ? '#000' : '#fff' }}>
+      <View style={{ padding: 16 }}>
+        <TouchableOpacity
+          onPress={toggleTheme}
+          style={{
+            backgroundColor: isDarkTheme ? '#fff' : '#000',
+            padding: 12,
+            borderRadius: 8,
+          }}
+        >
+          <Text
+            style={{ color: isDarkTheme ? '#000' : '#fff', textAlign: 'center', fontSize: 16 }}
+          >
+            Toggle Theme
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={randomPosts}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <PostCard isFeed={true} post={item} isDarkTheme={isDarkTheme} />
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default Recommendations;
